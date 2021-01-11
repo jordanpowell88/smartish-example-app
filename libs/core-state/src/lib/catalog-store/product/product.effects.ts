696d0c3b186b52629b0302918e8a1ff3ef251d64
Jordan <jordan@jpdesigning.com>
2021-01-11 11:50:43 -0500

    feat(web): save product

--- libs/core-state/src/lib/catalog-store/product/product.effects.ts
+++ libs/core-state/src/lib/catalog-store/product/product.effects.ts
@@ -1,47 +1,75 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { RouterNavigatedAction, ROUTER_NAVIGATED } from '@ngrx/router-store';
+import {
+  RouterNavigatedAction,
+  ROUTER_NAVIGATED,
+  SerializedRouterStateSnapshot,
+} from '@ngrx/router-store';
 import { ProductService } from './product.service';
-import { filter, map, tap } from 'rxjs/operators';
-import { setSelectedProductId } from './product.actions';
+import { catchError, exhaustMap, filter, map } from 'rxjs/operators';
+import {
+  setSelectedProductId,
+  updateProduct,
+  updateProductFailed,
+  updateProductSuccess,
+} from './product.actions';
+import { of } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class ProductEffects {
   constructor(
     private readonly actions$: Actions,
-    private readonly productService: ProductService
+    private readonly productService: ProductService,
+    private readonly router: Router
   ) {}
 
   setSelectedProductFromRoute$ = createEffect(() =>
     this.actions$.pipe(
       ofType(ROUTER_NAVIGATED),
-      filter((r: RouterNavigatedAction) =>
-        r.payload.routerState.url.includes('/catalog')
-      ),
-      filter(
-        (r: RouterNavigatedAction) =>
-          !!r.payload.routerState.root.firstChild.firstChild.params.productId
+      map((r: RouterNavigatedAction) => r.payload.routerState),
+      filter((routerState: SerializedRouterStateSnapshot) =>
+        routerState.url.includes('/catalog')
       ),
       map(
         ({
-          payload: {
-            routerState: {
-              root: {
-                firstChild: {
-                  firstChild: {
-                    params: { productId },
-                  },
-                },
+          root: {
+            firstChild: {
+              firstChild: {
+                params: { productId },
               },
             },
           },
         }) => productId
       ),
+      filter((productId) => !!productId),
       map((productId) =>
         setSelectedProductId({
           selectedId: productId,
         })
       )
     )
+  );
+
+  productPageSaveButtonClicked$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(updateProduct),
+      map((action) => action.product),
+      exhaustMap((product) =>
+        this.productService.update(product).pipe(
+          map((product) => updateProductSuccess({ product })),
+          catchError((error) => of(updateProductFailed({ error })))
+        )
+      )
+    )
+  );
+
+  routeToCatalogOnProductSaveSuccess$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(updateProductSuccess),
+        map(() => this.router.navigate(['/catalog']))
+      ),
+    { dispatch: false }
   );
 }
